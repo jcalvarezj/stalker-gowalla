d0ea08062a3417cdf8459fa944e8723d751a869a
J. Alvarez <jcalvarezj@gmail.com>
2021-02-25 20:10:23 -0500

    Separate IPython notebook in pure python scripts

---
+++ main.py
@@ -0,0 +1,152 @@
+import os
+from datetime import datetime
+
+DATA_FOLDER = './test/'#'./data/'
+EDGES_URL = 'https://snap.stanford.edu/data/loc-gowalla_edges.txt.gz'
+EDGES_COMP_FILE = 'loc-gowalla_edges.txt.gz'
+EDGES_FILE = 'edgestest.txt'#'loc-gowalla_edges.txt'
+CHECKINS_URL = 'https://snap.stanford.edu/data/loc-gowalla_totalCheckins.txt.gz'
+CHECKINS_COMP_FILE = 'loc-gowalla_totalCheckins.txt.gz'
+CHECKINS_FILE = 'checkinstest.txt'#'loc-gowalla_totalCheckins.txt'
+
+
+class Graph:
+    """
+    This class represents the relations between people
+    """
+    def __init__(self):
+        """
+        Constructor initializer
+        """
+        self.relations = {}
+        self.weights = {}
+
+    def add_relation(self, start_node, end_node):
+        """
+        Adds a relation between two nodes
+        """
+        if not self.relations.get(start_node):
+            self.relations[start_node] = {end_node}
+        else:
+            self.relations[start_node].add(end_node)
+
+    def add_weight(self, start_node, end_node, weight):
+        """
+        Adds a weight for a couple of nodes' association
+        """
+        pair = start_node, end_node
+        if not self.weights.get(pair):
+            self.weights[pair] = {weight}
+        else:
+            self.weights[pair].add(weight)
+
+    def __str__(self):
+        return f'Graph with nodes:\n{self.relations}\nAnd weights:\n{self.weights}'
+
+
+def obtain_data_files(url):
+    """
+    Uses Linux/Unix commands to retrieve the file from the specified URL
+    """
+    try:
+        os.system('echo "Downloading file...."')
+        os.system(f'wget {url} -P {DATA_FOLDER}')
+    except Exception as e:
+        print('Problem retrieving files')
+        print(e)
+
+
+def extract_data_files(filename):
+    """
+    Uses Linux/Unix commands to extract the retrieved files
+    """
+    try:
+        os.system(f'echo "Uncompressing {filename}"')
+        os.system(f'gzip -dkv {filename}')
+        os.system(f'gzip -dkv {filename}')
+    except Exception as e:
+        print(f'There was a problem when uncompressing the file {filename}')
+        print(e)
+
+
+def read_friendship_graph(friends_file):
+    """
+    Reads the edges (friendships) file and returns a graph with the associations
+    """
+    friendship_graph = Graph()
+
+    with open(friends_file) as edges:
+        for line in edges:
+            user, friend = line.split('\t')
+            user = int(user)
+            friend = int(friend.replace('\n', ''))
+            friendship_graph.add_relation(user, friend)
+
+    return friendship_graph
+
+
+def read_stalkers_graph(checkins_file):
+    """
+    Reads the check-ins file and returns a graph with the associations between people
+    that mean stalking (weighted as a list of involved location ids)
+    """
+    stalkers_graph = Graph()
+    visit_records = {}
+
+    with open(checkins_file) as checkins:
+        for line in checkins:
+            user_id, checkin_time, _, _, location_id = line.split('\t')
+            user_id = int(user_id)
+            checkin_time = checkin_time.replace('Z', '+00:00')
+            location_id = int(location_id.replace('\n', ''))
+
+            new_visit = (user_id, checkin_time)
+
+            if not visit_records.get(location_id):
+                visit_records[location_id] = [new_visit]
+            else:
+                for visit in visit_records[location_id]:
+                    if new_visit[1] < visit[1]:
+                        stalkers_graph.add_weight(new_visit[0], visit[0], location_id)
+                    else:
+                        stalkers_graph.add_weight(visit[0], new_visit[0], location_id)
+                
+                visit_records[location_id].append(new_visit)
+
+    return stalkers_graph
+
+
+def compute_most_stalking_people():
+    """
+    Answers the second question by calculating which stalker pair has the highest score
+    for a pair of people who are not friends to each other
+    """
+    highest_friend_stalker = (None, 0)
+    highest_nonfriend_stalker = (None, 0)
+    stalkers_graph = read_stalkers_graph(f'{DATA_FOLDER}{CHECKINS_FILE}')
+    friendship_graph = read_friendship_graph(f'{DATA_FOLDER}{EDGES_FILE}')
+    stalking_dict = stalkers_graph.weights
+
+    for i, pair_locations in enumerate(stalking_dict.items()):
+        pair, locations = pair_locations
+        stalking_score = len(locations)
+
+        if stalking_score > highest_friend_stalker[1]:
+            if pair[0] in friendship_graph.relations[pair[1]]:
+                highest_friend_stalker = (pair, stalking_score)
+            else:
+                highest_nonfriend_stalker = (pair, stalking_score)
+
+    return highest_friend_stalker, highest_nonfriend_stalker
+
+
+if __name__ == '__main__':
+    #obtain_data_files(EDGES_URL)
+    #obtain_data_files(CHECKINS_URL)
+    #extract_data_files(f'{DATA_FOLDER}{EDGES_COMP_FILE}')
+    #extract_data_files(f'{DATA_FOLDER}{CHECKINS_COMP_FILE}')
+    most_stalking_friend, most_stalking_nonfriend = compute_most_stalking_people()
+    print('The most stalking pair of non-friends is')
+    print(most_stalking_nonfriend)
+    print('The most stalking pair of non-friends is')
+    print(most_stalking_friend)
---
+++ test.py
@@ -0,0 +1,66 @@
+import unittest
+from main import Graph, read_friendship_graph, read_stalkers_graph
+from main import compute_most_stalking_people
+
+TEST_FOLDER = './test/'
+TEST_EDGES_FILE = 'edgestest.txt'
+TEST_CHECKINS_FILE = 'checkinstest.txt'
+
+
+class SolutionTest(unittest.TestCase):
+    """
+    Class to execute unit tests of the solution from test cases at './test'
+    """
+    def setUp(self):
+        """
+        Unit test initialization
+        """
+        self.friends_graph = Graph()
+        self.friends_graph.add_relation(1, 3)
+        self.friends_graph.add_relation(2, 4)
+        self.friends_graph.add_relation(3, 1)
+        self.friends_graph.add_relation(4, 2)
+
+        self.stalkers_graph = Graph()
+        self.stalkers_graph.add_weight(1, 2, 1)
+        self.stalkers_graph.add_weight(1, 2, 6)
+        self.stalkers_graph.add_weight(1, 3, 2)
+        self.stalkers_graph.add_weight(2, 3, 4)
+        self.stalkers_graph.add_weight(2, 4, 3)
+        self.stalkers_graph.add_weight(2, 1, 5)
+
+        highest_stalker_friend = ((1, 3), 1)
+        highest_stalker_nonfriend = ((1, 2), 2)
+        self.highest_stalker_people = highest_stalker_friend, highest_stalker_nonfriend
+
+    def tearDown(self):
+        """
+        Unit test clean-up
+        """
+        del(self.friends_graph)
+        del(self.stalkers_graph)
+
+    def test_read_friends_graph(self):
+        """
+        Test for the read_friendship_graph(...) method
+        """
+        computed_friends_graph = read_friendship_graph(f'{TEST_FOLDER}{TEST_EDGES_FILE}')
+        self.assertEqual(self.friends_graph.relations, computed_friends_graph.relations)
+
+    def test_read_stalkers_graph(self):
+        """
+        Test for the read_stalkers_graph(...) method
+        """
+        computed_stalkers_graph = read_stalkers_graph(f'{TEST_FOLDER}{TEST_CHECKINS_FILE}')
+        self.assertEqual(self.stalkers_graph.weights, computed_stalkers_graph.weights)
+
+    def test_compute_most_stalking_people(self):
+        """
+        Test for the compute_most_stalking_people() method
+        """
+        computed_highest_stalker_people = compute_most_stalking_people()
+        self.assertEqual(self.highest_stalker_friend, computed_highest_stalker_people)
+
+
+if __name__ == '__main__':
+    unittest.main()
